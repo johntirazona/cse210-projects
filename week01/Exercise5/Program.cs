d45642193d2e8b0ab2fcbc8f9644a281bf26de0d
johntirazona <johnjenlytirazona@gmail.com>
2025-11-01 10:51:31 +0800

    initial code for exercise 5 - inc

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquaredNumber(userNumber);
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int answer = Console.ReadLine();
+            number = int.Parse(answer);
+            return number;
+        }
+
+        static int SquaredNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
     }
 }
